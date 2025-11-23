acafab2c5a672212539d4f927741e96e4442f629
Lukas Lütke-Lengerich <lukas.luetke.ds@gmail.com>
2025-11-23 13:45:02 +0100

    implemented an easy version of mlp

--- src/models/MLP.py
+++ src/models/MLP.py
@@ -1,2 +1,4 @@
 # I wanna implement a simple multi layer perceptron just to have a good starting point 
-# into this project
+# into this project, the design process is documented in the jupyter notebook
+import numpy as np
+import torch
